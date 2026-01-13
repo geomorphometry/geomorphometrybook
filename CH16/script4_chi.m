ec7aab45003c586651c00f88f4586cc0970d4977
schwangh <wolfgang.schwanghart@uni-potsdam.de>
2026-01-13 09:54:17 +0100

    Update script4_chi.m

--- CH16/script4_chi.m
+++ CH16/script4_chi.m
@@ -1,47 +1,15 @@
-% Download DEM (Copernicus DEM) for specified extent in lat/lon 
-ext = [8.50   9.65   41.32   43.05];
-DEM = readopentopo("extent",ext,"demtype","COP90");
-
+% Read Ponui Island DEM
+DEM = GRIDobj("DEMs\ponui_island_dtm.tif");
 % Clip the DEM to values that are positive. Non-positive values are 
 % set to missing values (nan)
 DEM = clip(DEM,DEM>0);
-% Reproject to utm
-DEM = reproject2utm(DEM,90);
-% Interpolate missing values surrounded by valid values
-% using laplacian interpolation
-DEM = inpaintnans(DEM);
+DEM.Z(~bwareaopen(DEM.Z,1e6)) = nan;
 
 % Calculate flow directions
 FD  = FLOWobj(DEM);
-% Calculate flow accumulation
-A   = flowacc(FD);
 
 % Get stream network with a minimum upstream area of 1000 pixels
-S   = STREAMobj(FD,'minarea',1000);
-
-% The following code finds the main divide and generates the plots
-% shown in the chapter
-
-% Identify northern- and southernmost tip
-I = ~isnan(DEM);
-I.Z = bwareaopen(I.Z,1e5);
-DEM = clip(DEM,I);
-[r,c]    = find(DEM);
-
-[~,ixx] = min(r);
-I.Z(1:r(ixx),c(ixx)) = true;
-
-[~,ixx] = max(r);
-I.Z(r(ixx):end,c(ixx)) = true;
-
-RightBasins = imdilate(imclearborder(~I.Z,8,"Borders","left"),ones(3));
-LeftBasins = imdilate(imclearborder(~I.Z,8,"Borders","right"),ones(3));
-
-DRight = drainagebasins(FD,find(~isnan(DEM) & RightBasins));
-DLeft = drainagebasins(FD,find(~isnan(DEM) & LeftBasins));
-
-DIV   = dilate(DRight>0,ones(3)) & dilate(DLeft>0,ones(3));
-
+S   = STREAMobj(FD,'minarea',1e4,'unit','map');
 
 %% Plot
 subplot(1,3,[1 2])
@@ -50,10 +18,10 @@ mn = mnoptimvar(S,DEM,A,"distbins",50,"a0",1);
 yl = ylim;
 ylim([-10 yl(2)])
 
-c = chitransform(S,flowacc(FD),"mn",mn,'a0',1);
+c = chitransform(S,A,"mn",mn,'a0',1);
 
 subplot(1,3,3)
-imageschs(DEM,clip(dilate(DIV,ones(5)),~isnan(DEM)),'truecolor','k','colorbar',false);
+imageschs(DEM,'colormap',[1 1 1],'truecolor','k','colorbar',false);
 hold on
 plotc(S,c);
 colormap("turbo")
